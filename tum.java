90d70952ded843078cb46070146517dd744ad920
Nikhlesh7505 <nikhileshkm956@gmail.com>
2024-07-06 15:36:25 +0530

    hello

---
+++ tum.java
@@ -0,0 +1,59 @@
+class hum
+{
+	void show()
+	{
+	System.out.println("hello");
+	try
+	{	
+		
+		try
+		{
+			try
+
+			{
+				String z="shani";
+				System.out.println(z.toUpperCase());
+				System.out.println((z.Integer.parseInt());
+			}
+			catch(Exception c)
+			{
+			System.out.println("cann't enter null value   " + c);	
+			}		
+
+
+		int a=20,b=0,c;
+		c=a/b;
+		   
+		}
+
+		catch(ArithmeticException e)
+		{
+		System.out.println("cann't divide by zero    "+ e);
+		}
+
+
+	int n[]={4,4,67,45,23,4,5,67,89};
+	System.out.println(n[45]);
+	System.out.println("good morning");
+	}
+
+
+        catch(ArrayIndexOutOfBoundsException e)
+	{
+		
+
+	System.out.println("please maintain index value    "+ e );
+	}
+	
+}
+}
+
+class tum
+{
+	public static void main(String args[])
+	{
+	hum r=new hum();
+	r.show();
+	}
+}
+
